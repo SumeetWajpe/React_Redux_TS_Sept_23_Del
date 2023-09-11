f1e229a86c4e69835af584364fbddaa64b8f0c36
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-11 12:46:30 +0530

    ✨ Props as object

--- DAY-1/CRAbasics/my-app/src/App.js
+++ DAY-1/CRAbasics/my-app/src/App.js
@@ -4,17 +4,29 @@ import React from "react";
 import { Message } from "./components/message";
 
 class App extends React.Component {
+  messageOne = {
+    msg: "Hello",
+    from: "Joe",
+    to: "Jim",
+    imageUrl:
+      "https://t3.ftcdn.net/jpg/02/09/65/14/360_F_209651427_Moux8Hkey15wtMbtLymbPPrdrLhm58fH.jpg",
+  };
   render() {
     return (
       <div>
-        <Message
-          msg="Hello"
-          from="Joe"
-          to="Jim"
-          imageUrl="https://t3.ftcdn.net/jpg/02/09/65/14/360_F_209651427_Moux8Hkey15wtMbtLymbPPrdrLhm58fH.jpg"
+        <Message messagedetails={this.messageOne} />
+        {/* <Message
+          msg="Hi"
+          from="Loe"
+          to="Kim"
+          imageUrl="https://img.freepik.com/premium-vector/hi-word-speech-bubble-funny-lettering-typography-sticker-social-media-content_102918-48.jpg"
         />
-        {/* <Message msg="Hi" from="Loe" to="Kim" />
-        <Message msg="Hey" from="Joe" to="Lim" /> */}
+        <Message
+          msg="Hey"
+          from="Joe"
+          to="Lim"
+          imageUrl="https://cdn2.vectorstock.com/i/1000x1000/59/56/hey-comic-word-vector-15865956.jpg"
+        /> */}
       </div>
     );
   }
--- DAY-1/CRAbasics/my-app/src/components/message.jsx
+++ DAY-1/CRAbasics/my-app/src/components/message.jsx
@@ -5,14 +5,14 @@ export class Message extends React.Component {
     return (
       <div>
         <img
-          src={this.props.imageUrl}
-          alt={this.props.msg}
+          src={this.props.messagedetails.imageUrl}
+          alt={this.props.messagedetails.msg}
           height="200px"
           width="300px"
         />
-        <h2>{this.props.msg}</h2>
-        <h3>From : {this.props.from} </h3>
-        <h3>To : {this.props.to} </h3>
+        <h2>{this.props.messagedetails.msg}</h2>
+        <h3>From : {this.props.messagedetails.from} </h3>
+        <h3>To : {this.props.messagedetails.to} </h3>
       </div>
     );
   }
