88abe2b4de0ee5df9039f56d0f8b8c7814240bed
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-11 12:31:17 +0530

    ✨ using props for setting attribute values

--- DAY-1/CRAbasics/my-app/src/App.js
+++ DAY-1/CRAbasics/my-app/src/App.js
@@ -7,9 +7,14 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Message msg="Hello" />
-        <Message msg="Hi" />
-        <Message msg="Hey" />
+        <Message
+          msg="Hello"
+          from="Joe"
+          to="Jim"
+          imageUrl="https://t3.ftcdn.net/jpg/02/09/65/14/360_F_209651427_Moux8Hkey15wtMbtLymbPPrdrLhm58fH.jpg"
+        />
+        {/* <Message msg="Hi" from="Loe" to="Kim" />
+        <Message msg="Hey" from="Joe" to="Lim" /> */}
       </div>
     );
   }
--- DAY-1/CRAbasics/my-app/src/components/message.jsx
+++ DAY-1/CRAbasics/my-app/src/components/message.jsx
@@ -2,6 +2,18 @@ import React from "react";
 
 export class Message extends React.Component {
   render() {
-    return <h2>{this.props.msg}</h2>;
+    return (
+      <div>
+        <img
+          src={this.props.imageUrl}
+          alt={this.props.msg}
+          height="200px"
+          width="300px"
+        />
+        <h2>{this.props.msg}</h2>
+        <h3>From : {this.props.from} </h3>
+        <h3>To : {this.props.to} </h3>
+      </div>
+    );
   }
 }
