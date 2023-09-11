3cdac585ba77b04546d2de99836b898aab57f60c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-11 12:10:16 +0530

    ✨ Reusable & Composable Components

--- DAY-1/CRAbasics/my-app/src/App.js
+++ DAY-1/CRAbasics/my-app/src/App.js
@@ -1,10 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
+import { Message } from "./components/message";
 
 class App extends React.Component {
   render() {
-    return <h1>Hello CRA !</h1>;
+    return (
+      <div>
+        <Message />
+        <Message />
+        <Message />
+      </div>
+    );
   }
 }
 
---
+++ DAY-1/CRAbasics/my-app/src/components/message.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Message extends React.Component {
+  render() {
+    return <h2>Hello !</h2>;
+  }
+}
