bbb0648388359a8b5fadf6fd5cbf251949ed708e
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-12 15:00:15 +0530

    ✨ Strong Typing Functional Component

--- DAY-1/onlineshopping/src/components/functional/functional.message.tsx
+++ DAY-1/onlineshopping/src/components/functional/functional.message.tsx
@@ -10,4 +10,7 @@ type MessageProps = {
   msg: string;
 };
 
-export var Message = (props: MessageProps) => <h1>{props.msg}</h1>;
+// export var Message = (props: MessageProps) => <h1>{props.msg}</h1>;
+export var Message: React.FC<MessageProps> = (props: MessageProps) => {
+  return <h1>{props.msg}</h1>;
+};
