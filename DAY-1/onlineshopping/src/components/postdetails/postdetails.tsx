2f5955a80bc5483d2d69ea5ff5a9e15478f68c1c
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-13 14:30:54 +0530

    ✨ Consuming routing parameter in postdetails

--- DAY-1/onlineshopping/src/components/postdetails/postdetails.tsx
+++ DAY-1/onlineshopping/src/components/postdetails/postdetails.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function PostDetails() {
+  const { id } = useParams();
   return (
     <div>
       <header>
-        <h1>Post Details</h1>
+        <h1>Post Details for {id}</h1>
       </header>
     </div>
   );
