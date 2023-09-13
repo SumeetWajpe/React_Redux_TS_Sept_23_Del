176f7e3a2ca3d424bfad85f35d717f775ddae3c7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-13 15:01:26 +0530

    ✨ Dispalying post details

--- DAY-1/onlineshopping/src/components/postdetails/postdetails.tsx
+++ DAY-1/onlineshopping/src/components/postdetails/postdetails.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { PostModel } from "../../models/postmodel";
 
 export default function PostDetails() {
   const { id } = useParams();
+
+  const [post, setPost] = useState(new PostModel()); // using default values
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then(response => response.json())
+      .then(post => setPost(post));
+  });
   return (
     <div>
       <header>
         <h1>Post Details for {id}</h1>
       </header>
+      <main>
+        <section>
+          <div>
+            <h2>Title : {post.title}</h2>
+            <h4>Body : {post.body}</h4>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
--- DAY-1/onlineshopping/src/models/postmodel.ts
+++ DAY-1/onlineshopping/src/models/postmodel.ts
@@ -1,8 +1,8 @@
 export class PostModel {
   constructor(
-    public id: number,
-    public userId: number,
-    public title: string,
-    public body: string,
+    public id: number=0,
+    public userId: number=0,
+    public title: string="",
+    public body: string="",
   ) {}
 }
