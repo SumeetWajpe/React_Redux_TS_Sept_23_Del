6a28929d1ff6fb3bdc8ffb6b49b853ad25bcdd0b
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-13 12:40:21 +0530

    ✨ using link vs a tag

--- DAY-1/onlineshopping/src/components/app/App.tsx
+++ DAY-1/onlineshopping/src/components/app/App.tsx
@@ -5,10 +5,12 @@ import Posts from "../posts/posts";
 import GetPostById from "../getpostbyid/getpostbyid";
 import InputMessage from "../message/inputMessage";
 import NewProductWithRHF from "../newproduct/newproduct.reacthookform";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 const App: React.FC = () => {
   return (
     <BrowserRouter>
+      {/* <a href="/">Products</a> | <a href="/posts">Posts</a> */}
+      <Link to="/">Products</Link> | <Link to="/posts">Posts</Link>
       <Routes>
         <Route path="/" element={<ListOfProducts />} />
         <Route path="/posts" element={<Posts />} />
