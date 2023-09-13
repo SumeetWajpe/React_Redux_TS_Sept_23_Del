14f3b1a1707ccb7e7df93974d003c346cef9c46d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-13 11:32:06 +0530

    ✨ Using React Hook Form

--- DAY-1/onlineshopping/src/components/app/App.tsx
+++ DAY-1/onlineshopping/src/components/app/App.tsx
@@ -4,14 +4,16 @@ import ListOfProducts from "../listofproducts/listofproducts";
 import Posts from "../posts/posts";
 import GetPostById from "../getpostbyid/getpostbyid";
 import InputMessage from "../message/inputMessage";
+import NewProductWithRHF from "../newproduct/newproduct.reacthookform";
 
 const App: React.FC = () => {
   return (
     <>
-      <ListOfProducts />
+      {/* <ListOfProducts /> */}
       {/* <Posts /> */}
       {/* <GetPostById /> */}
       {/* <InputMessage /> */}
+      <NewProductWithRHF/>
     </>
   );
 };
---
+++ DAY-1/onlineshopping/src/components/newproduct/newproduct.reacthookform.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+type NewProductInputs = {
+  id: number;
+  title: string;
+  price: number;
+  rating: number;
+  likes: number;
+  imageUrl: string;
+};
+export default function NewProductWithRHF() {
+  const { register, handleSubmit } = useForm<NewProductInputs>();
+  return (
+    <div>
+      <h1>New Product</h1>
+      <div className="d-flex justify-content-center align-items-center">
+        <form
+          onSubmit={handleSubmit(newProduct => {
+            console.log(newProduct);
+          })}
+        >
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductId">Id : </label>
+            </div>
+            <div className="col-md-4">
+              <input type="number" id="txtProductId" {...register("id")} />
+            </div>
+          </div>
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductTitle">Title : </label>
+            </div>
+            <div className="col-md-4">
+              <input type="text" id="txtProductTitle" {...register("title")} />
+            </div>
+          </div>
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductPrice">Price : </label>
+            </div>
+            <div className="col-md-4">
+              <input
+                type="number"
+                id="txtProductPrice"
+                {...register("price")}
+              />
+            </div>
+          </div>
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductRating">Rating : </label>
+            </div>
+            <div className="col-md-4">
+              <input
+                type="number"
+                id="txtProductRating"
+                {...register("rating")}
+              />
+            </div>
+          </div>
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductLikes">Likes : </label>
+            </div>
+            <div className="col-md-4">
+              <input
+                type="number"
+                id="txtProductLikes"
+                {...register("likes")}
+              />
+            </div>
+          </div>
+          <div className="row my-1">
+            <div className="col-md-4">
+              <label htmlFor="txtProductImageUrl">Image : </label>
+            </div>
+            <div className="col-md-4">
+              <input
+                type="text"
+                id="txtProductImageUrl"
+                {...register("imageUrl")}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-9">
+              <button className="btn btn-success">
+                Add new product{" "}
+                <i className="fa fa-plus-circle" aria-hidden="true"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
