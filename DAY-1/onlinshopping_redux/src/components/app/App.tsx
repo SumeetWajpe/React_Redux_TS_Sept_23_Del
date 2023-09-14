ad158c745f2c0f875c6399fb60431c00d94095e1
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2023-09-14 11:49:46 +0530

    ✨ using provider | useSelector

--- DAY-1/onlinshopping_redux/src/components/app/App.tsx
+++ DAY-1/onlinshopping_redux/src/components/app/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function App() {
+  const products = useSelector((store: any) => store.products);
+  console.log(products);
   return <div>Using Redux</div>;
 }
 
